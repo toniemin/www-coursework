0d44d9a3d14fb5bb69a6f50ac50b451e964f8ca8
Topi Nieminen <budy426@gmail.com>
2019-05-19 15:37:01 +0300

    Started working on admin/mod user list.

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,7 @@ const path = require("path");
 const loginController = require("../access-control/login").login;
 const userController = require("../controllers/userController");
 const Thread = require("../model/model").Thread;
+const User = require("../model/model").User;
 
 router.get("/", (req, res) => {
   // Get all thread headlines from the database.
@@ -72,6 +73,29 @@ router.get("/logout", (req, res) => {
 
     res.redirect("/");
   });
+});
+
+router.get("/userList", (req, res) => {
+  let role = req.session.role;
+
+  if (!role in ["mod", "admin"]) {
+    res.redirect("/");
+    return;
+  }
+
+  let isAdmin = role === "admin";
+  User.find({}, (err, users) => {
+    res.render("userList", {
+      isAdmin,
+      users,
+      script: "../public/front_userList.js"
+    });
+  });
+});
+
+router.get("/public/front_userList.js", (req, res) => {
+  let filepath = path.join(__dirname, "..", "/public/front_userList.js");
+  res.sendFile(filepath);
 });
 
 router.get("/public/front_login.js", (req, res) => {
